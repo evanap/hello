f52c13b637000da32bede6cf60e0775e33c17ac4
Evan <evan.adiprasetyo@gmail.com>
2020-08-08 15:23:54 +0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type server struct{}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	helloByte := []byte(`{"message": "hello world"}`)
+	w.Write(helloByte)
+}
+
+func main() {
+	s := &server{}
+	http.Handle("/hello", s)
+	log.Fatal(http.ListenAndServe(":9081", nil))
+}
