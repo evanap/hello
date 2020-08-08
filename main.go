be5adc6dccae148460b252e3d0120e3e4989c2f7
Evan <evan.adirpasetyo@gmail.com>
2020-08-08 16:46:25 +0700

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ type server struct{}
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-	helloByte := []byte(`{"message": "hello world"}`)
+	helloByte := []byte(`{"message": "Hello, world"}`)
 	w.Write(helloByte)
 }
 
